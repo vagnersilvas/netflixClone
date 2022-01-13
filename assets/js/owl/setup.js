efac3cf9b3bc8d4b58f8e0eef5597cb4bea8b2a4
vagner da silva <vagnrdaia@gmail.com>
2022-01-12 22:46:14 -0300

    arquivos principais

---
+++ assets/js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2.5
+        },
+        600:{
+            items:4.5
+        },
+        1000:{
+            items:6.5
+        }
+    }
+})
